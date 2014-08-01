17b172e90f99e2ce51662f4c7688651e2a2a53b3
borna <jmihalje2@ffzg.hr>
2014-08-01 14:02:25 +0200

    Main menu

---
+++ Assets/kod/backtoTitle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backtoTitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            Application.LoadLevel("title");
+            var glazba = GameObject.Find("One shot audio");
+            DestroyObject(glazba);
+        }
+	}
+}
---
+++ Assets/kod/mouseEnter.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseEnter : MonoBehaviour
+{
+
+    // Use this for initialization
+    private TextMesh tm;
+    private Renderer textRenderer;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        Screen.showCursor = true;
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+
+    private void OnMouseEnter()
+    {
+        renderer.material.color = Color.gray;
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "exit")
+            Application.Quit();
+
+        if (gameObject.name == "options")
+        {
+            Application.LoadLevel("options");
+            var glazba = GameObject.Find("main_music");
+            DontDestroyOnLoad(glazba);
+        }
+
+
+        if (gameObject.name == "back")
+        {
+            Application.LoadLevel("menu");
+            var glazba = GameObject.Find("main_music");
+            DontDestroyOnLoad(glazba);
+        }
+
+        if (gameObject.name == "new_game")
+            {
+                Application.LoadLevel("level1");
+            }
+        }
+
+
+        void OnMouseExit ()
+        {
+
+            renderer.material.color = Color.white;
+        }
+
+
+    
+}
