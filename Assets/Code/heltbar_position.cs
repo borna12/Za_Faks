2add581d6c3a970577103dcfc7d552eda701d050
borna <jmihalje2@ffzg.hr>
2014-08-05 00:17:58 +0200

    helth bar u ljevom kutu sljedi kameru

---
+++ Assets/Code/heltbar_position.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heltbar_position : MonoBehaviour
+{
+    public GameObject objectTarget;
+    public Vector3 screenPosition = new Vector3(0, 0, 20);
+
+    // Use this for initialization
+    void Update()
+    {
+
+        if (objectTarget != null)
+        {
+            objectTarget.transform.position = camera.ScreenToWorldPoint(screenPosition);
+        }
+
+    }
+}
